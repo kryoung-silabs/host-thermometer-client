b9b6a06d14f42454321fbf1982a27274da37826d
Kris Young <kris.young@silabs.com>
2019-09-07 10:49:42 -0400

    Updated deprecated api (le_gap_set_conn_timing_parameters)

--- app.c
+++ app.c
@@ -159,10 +159,12 @@ void appHandleEvents(struct gecko_cmd_packet *evt)
         // Set scan interval and scan window
         gecko_cmd_le_gap_set_discovery_timing(le_gap_phy_1m, SCAN_INTERVAL, SCAN_WINDOW);
         // Set the default connection parameters for subsequent connections
-        gecko_cmd_le_gap_set_conn_parameters(CONN_INTERVAL_MIN,
+        gecko_cmd_le_gap_set_conn_timing_parameters(CONN_INTERVAL_MIN,
                                              CONN_INTERVAL_MAX,
                                              CONN_SLAVE_LATENCY,
-                                             CONN_TIMEOUT);
+                                             CONN_TIMEOUT,
+                                             0,
+                                             0xffff);
         // Start scanning - looking for thermometer devices
         gecko_cmd_le_gap_start_discovery(le_gap_phy_1m, le_gap_discover_generic);
         connState = scanning;
